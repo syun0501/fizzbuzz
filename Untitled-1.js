e5c938f4ad63b06e7b573cbf6807edb0153384db
syun <konnnichiha0501@gmail.com>
2019-03-15 21:08:09 +0900

    kimさん

---
+++ Untitled-1.js
@@ -0,0 +1,11 @@
+for (var i =1 ; i <= 100; i++){
+
+    if((i%6)==0){document.write("fizz-buzz".fontcolor("purple")) }
+  
+    else if((i%3)==0){document.write("buzz".fontcolor("blue"))}
+  
+  else if((i%2)==0){document.write("fizz".fontcolor("red"))}
+  
+    else{document.write(i)}
+  
+  }
